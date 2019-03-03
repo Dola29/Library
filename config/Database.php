e01b44f35fb1dc26330b7d2b02ad1e8a799baa20
Dola29 <danielosvaldoleal@hotmail.com>
2019-03-03 17:28:44 -0400

    Se agrego el archivo de conexion a base de datos

---
+++ config/Database.php
@@ -0,0 +1,28 @@
+<?php 
+
+class Database {
+  // DB Parametros
+  private $host = 'localhost';
+  private $db_name = 'library';
+  private $username = 'root';
+  private $password = '';
+
+  private $conn;
+
+  // DB conectar
+  public function connect() {
+    $this->conn = null;
+
+    try{
+      $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->db_name,$this->username,$this->password);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    }catch(PDOException $e){
+      echo 'Error de conexion:' . $e->getMessage();
+    }
+    return $this->conn;
+  }
+
+  
+}
+?>
