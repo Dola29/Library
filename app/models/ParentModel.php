2b89e35ee3f8a7488ac5b81bc36a65f519faaac4
Dola29 <danielosvaldoleal@hotmail.com>
2019-03-03 17:30:12 -0400

    Se ceo el modelo Padre

---
+++ app/models/ParentModel.php
@@ -0,0 +1,63 @@
+<?php 
+include_once '../Library/classes/Database.php';
+
+class ParentModel {
+
+    public $conn;
+    public $table;
+    
+    public function __construct($table) {
+        $db = new Database();
+        $this->conn = $db->connect();
+        $this->table= $table;
+    }
+
+
+    /* recibe un string con los campos a seleccionar
+       y retorna todos los registros de la tabla 
+    */
+    public function get_all($fields) {
+        // Create query
+        $query = "SELECT  {$fields} FROM {$this->table}";
+        
+        //Preparar statement
+        $stmt = $this->conn->prepare($query);
+
+        // Ejecutar query
+        $stmt->execute();
+        
+        //se convierte la data en un arreglo
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        //retorna la data
+        return $data;
+    }
+    
+    /* recibe un string con los campos a seleccionar y el id del registro
+    y retorna un registro de la tabla 
+    */
+    public function get_single($fields, $id) {
+        // Crear query
+        $query = 'SELECT '.$fields.'FROM '.
+                $this->table .'WHERE id = ?';
+
+        // Preparar statement
+        $stmt = $this->conn->prepare($query);
+
+        // Enlazar ID
+        $stmt->bindParam(1, $id);
+
+        // Ejecutar query
+        $stmt->execute();
+
+        //se convierte el registro en un arreglo
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        // llenar propiedades
+        return $row;         
+    }
+      
+
+}
+
+?>
